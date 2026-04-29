dfa77815a54d216b38c8585e529d8fc19d1eaa8f
Jelle Weijland <j.p.weijland@gmail.com>
2026-04-29 15:41:39 +0200

    Fix nested repo

---
+++ Morris/Morris/MorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MorrisApp.swift
+//  Morris
+//
+//  Created by Jelle Weijland on 29/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct MorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
